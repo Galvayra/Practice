902596a0ec913a5c85f8908c41cfbca7ea6f152d
Galvayra <voltexlicer@gmail.com>
2019-08-03 17:32:06 +0900

    training cnn using cifar

---
+++ keras/4_step.py
@@ -0,0 +1,18 @@
+# CNN for cifar
+# from Practice.keras.Models import NeuralNet
+from keras import datasets
+from keras.backend import image_data_format
+from Practice.keras.Models import Machine
+
+assert image_data_format() == 'channels_last'
+
+
+class Step4(Machine):
+    def __init__(self):
+        (X, y), (_, _) = datasets.cifar10.load_data()
+        super().__init__(X, y, nb_classes=10)
+
+
+if __name__ == '__main__':
+    m = Step4()
+    m.run()
